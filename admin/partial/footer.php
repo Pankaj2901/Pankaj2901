e88ac9d5aa97c6ff0eff381c6b4f0f5acfc69fae
Pankaj2901 <77991163+Pankaj2901@users.noreply.github.com>
2022-06-14 15:41:00 +0530

    Add files via upload

---
+++ admin/partial/footer.php
@@ -0,0 +1,10 @@
+      <!-- footer section start -->
+      <div class="footer" style="background-color: coral; color: white;" >                          
+          <div class="wrapper"> 
+              <p class="text-center">All rights reserved of wow food. Designed By <a href="https://www.instagram.com/invites/contact/?i=f0lc38wrogpa&utm_content=htxlxnl">Deepak jaiswal</a></p>
+          </div>
+
+      </div>
+     <!-- footer  section end -->
+     </body>
+</html>
---
+++ admin/partial/menu.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <title>food order website - home page</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+    <!-- menu section starts -->
+    <div class ="menu text center"> 
+       <div class="wrapper">  
+          <ul style="list-style-type: none;  text-align: center; display: inline; ">
+             <li > <a style="color: orange " href="index.php"> home</a> 
+              <a style="color: orange;" href="manage-admin.php"> admin</a> 
+              <a style="color: orange" href="manage-category.php"> category</a> 
+              <a style="color: orange" href="manage-food.php"> food</a> 
+              <a style="color: orange" href="manage-order.php"> order</a> </li> 
+          </ul>
+           </div>
+
+       </div>
+     <!-- menu section end -->
